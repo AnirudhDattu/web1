db86c5d71976462063f968bd4f315d64c76cf06a
Anirudh Dattu <anirudh.dattu.22cse@bmu.edu.in>
2023-11-28 14:59:53 +0530

    login page addition

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-<script>
-    const scrollToTopButton = document.getElementById("scrollToTopButton");
-    scrollToTopButton.addEventListener("click", () = {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth" // This creates a smooth scrolling effect
-        });
-    });
-</script>
+function validate(event) {
+    event.preventDefault();
+    const emailInput = document.querySelector('#username');
+    const passwordInput = document.querySelector('#password');
+    const emailValue = emailInput.value.trim();
+    const passwordValue = passwordInput.value.trim();
+
+    console.log('Email:', emailValue);
+    console.log('Password:', passwordValue);
+
+    if (emailValue !== 'anirudh' || passwordValue !== '111') {
+        alert('Invalid email or password');
+        return false;
+    }
+
+    console.log('Redirecting to home.html');
+    // Redirect to another page on successful login
+    window.location.href = 'home.html';
+}
+
+const submitButton = document.querySelector('.login__submit');
+submitButton.addEventListener('click', validate);
